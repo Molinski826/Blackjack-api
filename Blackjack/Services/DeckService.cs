38e42d61b6c2fabbe3996e1ab468d7ccf180448a
Joey Molinski <molinski826@msn.com>
2024-09-30 13:56:14 -0400

    first commit

---
+++ Blackjack/Services/DeckService.cs
@@ -0,0 +1,25 @@
+﻿using Blackjack.Models;
+
+namespace Blackjack.Services
+{
+    public class DeckService
+    {
+        private readonly HttpClient _httpClient;
+        public DeckService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://www.deckofcardsapi.com/api/deck/");
+        }
+
+        public async Task<DeckModel> NewDeck()
+        {
+            DeckModel result = await _httpClient.GetFromJsonAsync<DeckModel>("new/shuffle");
+            return result;
+        }
+        public async Task<DeckModel> DrawCards(int count, string DeckId)
+        {
+            DeckModel result = await _httpClient.GetFromJsonAsync<DeckModel>($"{DeckId}/draw/?count={count}");
+            return result;
+        }
+    }
+}
